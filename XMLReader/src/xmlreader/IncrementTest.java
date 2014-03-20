becccb50951d7aa03a458d4b7818039db42adcfe
henri <henri@localhost>
2014-03-20 21:51:56 +0200

    Merge origin/master

---
+++ XMLReader/src/xmlreader/IncrementTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmlreader;
+
+/**
+ *
+ * @author henri
+ */
+public class IncrementTest {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = a++; // b = 0; a = 1
+        System.out.println(a);
+        System.out.println(b);
+        int c = 0;
+        int d = ++c: // b = 1; a = 1
+        System.out.println(c);
+        System.out.println(d);
+        
+        
+
+
+
+
+    }
+}
