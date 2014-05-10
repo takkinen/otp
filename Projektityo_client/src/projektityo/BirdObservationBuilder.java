458dbccfc5da3286c18eb1474a6e9f6b864319ba
henri <henri@ubuntu-laptop>
2014-05-10 15:20:03 +0300

    2014-05-10

---
+++ Projektityo_client/src/projektityo/BirdObservationBuilder.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projektityo;
+
+import java.sql.Date;
+import java.sql.Time;
+
+/**
+ *
+ * @author henri
+ */
+public class BirdObservationBuilder {
+    
+    private GeoPoint geoPoint;
+    private int observerID = 0000000000;
+    private String species;
+    private Date date;
+    private Time time;
+    DataHandler dataHandler;
+    
+    public BirdObservationBuilder() {}
+    
+    public void defineCurrentGeoPoint() {
+        //
+        geoPoint = DataHandler.getGeoPoint();
+    }
+    
+    public void setCurrentObserverID(int id) {
+        observerID = id;
+    }
+    
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    
+    public void setCurrentDate() {
+        date = new Date(System.currentTimeMillis());
+    }
+    
+    public void setCurrentTime() {
+        time = new Time(System.currentTimeMillis());
+    }
+    
+    public void createBirdObservation() {
+        if (geoPoint == null) { return; }
+        if (observerID <= 0) { return; }
+        if (date == null) { return; }
+        if (time == null) { return; }
+        if (species.length() > 0) {
+            BirdObservation birdObservation = new BirdObservation(observerID, date, time, geoPoint, species);
+            BirdObservationSender.sendBirdObservation(birdObservation);
+            System.out.println(birdObservation.toXML());
+            
+        }
+    }
+    
+    /*
+    public int getCurrentObserverID() {
+        return observerID;
+    }
+    
+    public GeoPoint getCurrentGeoPoint() {
+        // Get coordinates
+        
+        int latit = 24965714;
+        int longit = 64812394;
+        try {
+        GeoPoint gp = new GeoPoint(latit, longit);
+        return gp;
+        } catch (Exception ex) {
+            System.err.println(ex.toString());
+        }
+        return null;
+    }
+    */
+    
+    
+    
+    
+    
+}
---
+++ Projektityo_client/src/projektityo/BirdObservationSender.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projektityo;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.io.*;
+
+/**
+ *
+ * @author henri
+ */
+public class BirdObservationSender {
+    public static int SOCKET = 7890;
+    
+    public static void sendBirdObservation(BirdObservation bo) {
+        try (Socket socket = new Socket("localhost", 7890)) {
+            Writer out = new OutputStreamWriter(
+            socket.getOutputStream(), "UTF-8");
+            out.write(bo.toXML());
+            out.flush();
+            socket.shutdownOutput();
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+        }
+                
+    }
+    
+}
+
+
+/*
+try (Socket connection = new Socket("www.oreilly.com", 80)) {
+Writer out = new OutputStreamWriter(
+connection.getOutputStream(), "8859_1");
+out.write("GET / HTTP 1.0\r\n\r\n");
+out.flush();
+connection.shutdownOutput();
+// read the response...
+} catch (IOException ex) {
+ex.printStackTrace();
+}
+*/
